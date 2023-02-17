caa51e1649ad5a4883ddfbda2243724ca04acd8f
LordWinNgoran <cyrille4011@gmail.com>
2023-02-17 01:09:42 +0000

    DI-Bootcamp Week7-Day5 Mini-Project XP

---
+++ src/project/quiz/com/Main.java
@@ -0,0 +1,15 @@
+//Mini Project - Quiz Game
+/*
+ * @author: N'goran Kouadio Jean Cyrille
+ * @description: Creating a simple quiz game in Java that implements the OOP approach
+ * Date : 10/02/2023
+ */
+package project.quiz.com;
+
+
+// class Main call the Question of the Game
+public class Main {
+    public static void main(String[] args) {
+        new Question().play();
+    }
+}
